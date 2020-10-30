e1686ba80ea074cd58b9a4c252f3ad70b14d53ec
Fraida Fund <ffund@nyu.edu>
2020-10-30 14:35:36 -0400

    Create file-sender.py

---
+++ lab6/file-sender.py
@@ -0,0 +1,21 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+sock.connect(('10.10.2.100', 4000))
+
+f = open('file.txt', "rb")
+
+b = f.read(4096)
+
+bytes_sent = 0
+
+while (b):
+    sock.sendall(b)
+    bytes_sent += 4096
+    print("Sent %d out of 5767184 bytes" % bytes_sent)
+    b = f.read(4096)
+
+sock.shutdown(socket.SHUT_RDWR)
+
+f.close()
