74d63fbb128af1489e40bd2b007c49be3faac63d
Fraida Fund <ffund@nyu.edu>
2023-10-13 13:36:59 -0400

    Add no-offload script to repo

---
+++ scripts/no-offload.sh
@@ -0,0 +1,10 @@
+# Get a list of all experiment interfaces, excluding loopback
+ifs=$(netstat -i | tail -n+3 | grep -Ev "lo" | cut -d' ' -f1 | tr '\n' ' ')
+
+# Turn off offloading of all kinds, if possible!
+for i in $ifs; do 
+  sudo ethtool -K $i gro off  
+  sudo ethtool -K $i lro off  
+  sudo ethtool -K $i gso off  
+  sudo ethtool -K $i tso off
+done
